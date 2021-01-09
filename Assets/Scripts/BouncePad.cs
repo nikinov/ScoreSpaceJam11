b7decb21a4edc6825e91a58e988c8c3838e9a9bc
cbmcruz <cbmcruz.31@gmail.com>
2021-01-09 02:51:20 -0700

    Added bounce pad and made change to portals to avoid edge cases.

---
+++ Assets/Scripts/BouncePad.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BouncePad : MonoBehaviour
+{
+  public float bounceStrength = 100.0f;
+
+  private void OnTriggerEnter(Collider other)
+  {
+    Rigidbody hitRigidbody = other.transform.GetComponent<Rigidbody>();
+    hitRigidbody.AddForce(transform.up * hitRigidbody.velocity.magnitude * bounceStrength);
+  }
+}
